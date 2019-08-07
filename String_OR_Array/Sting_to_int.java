e6712143be0a83128743241a93cdf65457a713a9
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2019-08-07 17:40:57 +0600

    str to int

---
+++ String_OR_Array/Sting_to_int.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+package a;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mahbub
+ */
+
+
+package a;
+
+public class String_to_int {
+
+     public static int str_to_int(String str) {
+	if (str == null || str.length() < 1)
+		return 0;
+ 
+	// trim white spaces
+	str = str.trim();
+        
+       char flag = '+';
+ 
+	// check negative or positive
+	int i = 0;
+	if (str.charAt(0) == '-') {
+		flag = '-';
+		i++;
+	} else if (str.charAt(0) == '+') {
+		i++;
+	}
+	// use double to store result
+	double result = 0;
+ 
+	// calculate value
+	while (str.length() > i && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+		result = result * 10 + (str.charAt(i) - '0');
+		i++;
+	}
+ 
+	if (flag == '-')
+		result = -result;
+ 
+	// handle max and min
+	if (result > Integer.MAX_VALUE)
+		return Integer.MAX_VALUE;
+ 
+	if (result < Integer.MIN_VALUE)
+		return Integer.MIN_VALUE;
+ 
+	return (int) result;
+}
+    public static void main(String[] args) {
+        String s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a string");
+        s = sc.nextLine();
+        System.out.println(s);
+        int a = str_to_int(s);
+        System.out.println(a);
+    }
+    
+}
